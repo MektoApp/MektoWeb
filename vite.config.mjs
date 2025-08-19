357b0e3cb40ec72f71a2695c7829ffafacd54dad
luizallievi <crmterc@gmail.com>
2025-08-19 17:25:50 -0300

    chore: mekto

--- vite.config.mjs
+++ vite.config.mjs
@@ -42,6 +42,7 @@ export default defineConfig(() => {
     server: {
       port: 3000,
       host: true,
+      allowedHosts: ['mekto.com.br'],
       proxy: {
         // https://vitejs.dev/config/server-options.html
       },
