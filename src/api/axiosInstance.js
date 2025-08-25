63ce4eee4fadb4350bb68728e6ca026baabf3c3b
LuizAllievi <luizallievi@gmail.com>
2025-08-25 15:05:20 -0300

    a

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export const API_BASE_URL = 'https://api.mekto.com.br/api';
 
+
 const api = axios.create({
   baseURL: `${API_BASE_URL}`, // URL base da API
   headers: {
